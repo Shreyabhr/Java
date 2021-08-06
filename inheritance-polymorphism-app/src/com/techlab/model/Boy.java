58063423f2e48e091eca80e4e179f3a8930a4b8b
Shreyabhr <59743020+Shreyabhr@users.noreply.github.com>
2021-08-06 17:43:18 +0530

    Add files via upload

---
+++ inheritance-polymorphism-app/src/com/techlab/model/Boy.java
@@ -0,0 +1,13 @@
+package com.techlab.model;
+
+public class Boy extends Man{
+	
+	public void play() {
+		System.out.println("Boy is eating");
+	}
+	
+	public void eat() {
+		System.out.println("Boy is playing");
+	}
+
+}
---
+++ inheritance-polymorphism-app/src/com/techlab/model/Kids.java
@@ -0,0 +1,14 @@
+package com.techlab.model;
+
+public class Kids extends Man{
+	
+	public void play() {
+		System.out.println("Kid is eating");
+	}
+	
+	public void eat() {
+		System.out.println("Kid is playing");
+	}
+
+
+}
