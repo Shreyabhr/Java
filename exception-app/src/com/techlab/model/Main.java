fcc28b077b2255b37b71a1a1d6dcd054b8dc827e
Shreyabhr <59743020+Shreyabhr@users.noreply.github.com>
2021-08-06 18:02:57 +0530

    Add files via upload

---
+++ exception-app/src/com/techlab/model/Main.java
@@ -0,0 +1,9 @@
+package com.techlab.model;
+
+public class Main {
+
+	public static void main(String[] args) {
+		main(args);
+	}
+
+}
---
+++ exception-app/src/com/techlab/test/ExceptionTest.java
@@ -0,0 +1,12 @@
+package com.techlab.test;
+
+public class ExceptionTest {
+	public static void main(String[] args) {
+		int a = 6;
+		int b = 0;
+		int c = a/b;
+		System.out.println(c);
+		System.out.println("End of main");
+	}
+
+}
